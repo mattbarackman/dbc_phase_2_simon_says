9bac50d493c210a022f3a694be7048bec43c5e6f
mattbarackman <matthew.barackman@gmail.com>
2013-06-18 20:30:43 -0700

    reorganized js

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
 
+  function add_colored_square(response) {
+    var color = response.color;
+    var cell = response.cell;
+    $('li:nth-of-type(' + cell + ')').css('background-color', color);
+  }
+
   $('#get_color').on('click', function() {
-    var data = $.ajax({
+    $.ajax({
       url: '/color',
       method: 'POST',
       dataType: 'json'
@@ -9,10 +15,4 @@ $(document).ready(function() {
       add_colored_square(response);
     });
   });
-
-  function add_colored_square(response) {
-    var color = response.color;
-    var cell = response.cell;
-    $('li:nth-of-type(' + cell + ')').css('background-color', color);
-  }
 });
